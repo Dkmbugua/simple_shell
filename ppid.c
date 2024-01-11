316442ad07b731daa2764e38ae270d5c73cab1df
Dkmbugua <dkmbugua70@gmail.com>
2024-01-10 17:19:36 -0800

    simple_shell

--- ppid.c
+++ ppid.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main() {
+int main(void)
+{
 pid_t pid = getpid();
 pid_t ppid = getppid();
 printf("Current process PID: %d\n", pid);
 printf("Parent process PID: %d\n", ppid);
-return 0;
+return (0);
 }
