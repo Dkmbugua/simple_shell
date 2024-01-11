4edb09eda6e12e12deb6e644bdac2ea8dc084ba9
Dkmbugua <dkmbugua70@gmail.com>
2024-01-11 04:37:44 -0800

    simple_shell

--- forka.c
+++ forka.c
@@ -1,22 +1,32 @@
-/**
- * main - fork example
- *
- * Return: Always 0.
- */
-int main(void)
-{
-pid_t my_pid;
-pid_t pid;
+#include  <stdio.h>
+#include <unistd.h>
 
-printf("Before fork\n");
-pid = fork();
-if (pid == -1)
+/**
+ * custom_fork_example- demonstrate the fork system call
+ * return 0 on success and 1 on failure
+*/
+
+int custom_fork_example(void)
 {
-perror("Error:");
+pid_t my_process_id;
+pid_t child_process_id;
+
+printf("before forking\n");
+child_process_id = fork();
+
+if (child_process_id == -1)
+{
+perror("fork error:");
 return (1);
 }
-printf("After fork\n");
-my_pid = getpid();
-printf("My pid is %u\n", my_pid);
+printf("after forking\n");
+my_process_id = getpid();
+printf("my process id is %u\n", my_process_id);
+return (0);
+}
+
+int main(void)
+{
+custom_fork_example();
 return (0);
 }
