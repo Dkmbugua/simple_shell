e1f7112e9a82dd0cb35ce8b589a2ab58acb6949c
Dkmbugua <dkmbugua70@gmail.com>
2024-01-15 02:06:31 -0800

    simple_shell

---
+++ shell.h
@@ -0,0 +1,22 @@
+#ifndef _SHELL_H_
+#define _SHELL_H_
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void print_tokens(char *str, char *delim)
+{
+char *token = strtok(str, delim);
+char *start = str;
+
+int custom_fork_example(void)
+int main(void)
+{
+char *program = "ls";
+char *arg1 = "-l";
+char *arg2 = "/usr/";
+char *argv[] = {program, arg1, arg2, NULL};
+
+int main(int argcount, char **argvector)
+#endif
